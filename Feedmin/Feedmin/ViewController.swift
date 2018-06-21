7750677d371b3e62eb509c0b560669642bb6a6b4
戸上　祐希 <togami@j-online.jp>
2018-06-21 12:08:10 +0800

    初期コミット

---
+++ Feedmin/Feedmin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Feedmin
+//
+//  Created by 戸上　祐希 on 2018/06/21.
+//  Copyright © 2018年 Togami Yuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
